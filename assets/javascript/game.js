54f01a9ddae3f9f953e7ea69a200393db01dda9c
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-08-16 13:04:21 -0700

    character objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+var characters = {
+    "Rey": {
+        name: "Rey",
+        health: 120,
+        attack: 8,
+        imageUrl: "assets/images/rey.jpg",
+        enemyAttackBack: 15
+    },
+    "Finn": {
+        name: "Finn",
+        health: 100,
+        attack: 14,
+        imageUrl: "assets/images/finn.jpg",
+        enemyAttackBack: 5
+    },
+    "Kylo-Ren": {
+        name: "Kylo-Ren",
+        health: 150,
+        attack: 8,
+        imageUrl: "assets/images/kylo-ren.jpg",
+        enemyAttackBack: 5
+    },
+    "Captain Phasma": {
+        name: "Captain Phasma",
+        health: 180,
+        attack: 7,
+        imageUrl: "assets/images/captain-phasma.jpg",
+        enemyAttackBack: 25
+    }
+};
+console.log(characters);
+
+var renderCharacters = function(charObj, areaRender){
+    if(arearender === "#charact")
+}
+
+//Functions
+
+
+
+    $("character").on("click", function() {
+        console.log("click")
+    });
+
+
+});
+
+
+
+
+
+
+//Game 
